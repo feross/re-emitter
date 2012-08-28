a3f204f650fe71727326de2a36a480894edd21c3
Raynos <raynos2@gmail.com>
2012-08-27 20:26:15 -0700

    first

---
+++ index.js
@@ -0,0 +1,28 @@
+var slice = Array.prototype.slice
+    , EventEmitter = require("events").EventEmitter
+
+module.exports = ReEmitter
+
+function ReEmitter(other, list) {
+    var emitter = new EventEmitter()
+
+    list.forEach(reemit, {
+        emitter: emitter
+        , other: other
+    })
+
+    return emitter
+}
+
+function reemit(name) {
+    var emitter = this.emitter
+        , other = this.other
+
+    other.on(name, emit)
+
+    function emit() {
+        var args = slice.call(arguments)
+        args.unshift(name)
+        emitter.emit.apply(emitter, args)
+    }
+}
