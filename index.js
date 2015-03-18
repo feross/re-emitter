c1e5ea861d1a454be4477223fcc692d2dffd099e
Gleb Arestov <gr1b0k@gmail.com>
2015-03-18 21:20:42 +0300

    allow to cancel reemiting

--- index.js
+++ index.js
@@ -5,18 +5,25 @@ var EventEmitter = require('events').EventEmitter
 
 function reemit (source, target, events) {
   if (!Array.isArray(events)) events = [ events ]
+  var result = []
 
   events.forEach(function (event) {
-    source.on(event, function () {
+    var callback = function () {
       var args = [].slice.call(arguments)
       args.unshift(event)
       target.emit.apply(target, args)
+    }
+    source.on(event, callback)
+    result.push(function(){
+      source.removeListener(event, callback)
     })
   })
+
+  return result;
 }
 
 function filter (source, events) {
   var emitter = new EventEmitter()
-  reemit(source, emitter, events)
+  emitter._destroyers = reemit(source, emitter, events)
   return emitter
 }
